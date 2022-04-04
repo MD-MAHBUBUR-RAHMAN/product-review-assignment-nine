5b3e40d29c2a2ba88bbc77cbd621c45fd3fa7372
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-04-04 10:58:59 +0600

    chart edit

--- src/Components/Dashbord/Dashbord.js
+++ src/Components/Dashbord/Dashbord.js
@@ -55,8 +55,8 @@ const Dashbord = () => {
       <h1 className="text-lg text-center font-sans font-semibold text-emerald-600">
         Chart For Presentation
       </h1>
-      <div className="grid grid-cols-2 ml-5 mt-8">
-        <LineChart width={500} height={400} data={data}>
+      <div className="grid md:grid-cols-2 ml-5 mt-8">
+        <LineChart width={400} height={300} data={data}>
           <Line type="monotone" dataKey="investment" stroke="#8884d8" />
           <Line type="monotone" dataKey="revenue" stroke="#1663Be" />
           <CartesianGrid stroke="#ccc" />
@@ -65,7 +65,7 @@ const Dashbord = () => {
           <Tooltip />
         </LineChart>
 
-        <BarChart width={500} height={400} data={data}>
+        <BarChart width={400} height={300} data={data}>
           <Bar dataKey="revenue" fill="#8884d8" />
           <Bar dataKey="investment" fill="#1663Be" />
           <CartesianGrid stroke="#ccc" />
